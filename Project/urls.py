dbe6fce6037482e1217c4a66dda9f37eb4c0cfca
Naida <72371416+Naida33@users.noreply.github.com>
2021-09-30 17:28:46 +0800

    Update urls.py

--- Project/urls.py
+++ Project/urls.py
@@ -20,12 +20,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('sags', views.sags, name='sags'),
     path('foot', views.foot, name='foot'),
     path('chess', views.chess, name='chess'),
     path('admin/', admin.site.urls),
     path('', include('App.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
    
 ]
 
